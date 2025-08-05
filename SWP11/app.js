ea662405f7062387a1fab8f2dc878d79432f7fbb
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-05 21:40:31 +0530

    async function searchWikipedia is created

--- SWP11/app.js
+++ SWP11/app.js
@@ -5,3 +5,17 @@ const searchResults = document.getElementById("search-results");
 // Theme toggler elements
 const themeToggler = document.getElementById("theme-toggler");
 const body = document.body;
+
+async function searchWikipeida(query) {
+  const encodedQuery = encodeURIComponent(query);
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${encodedQuery}`;
+
+  const reponse = await fetch(endpoint);
+
+  if (!reponse.ok) {
+    throw new Error("Faild to fetch search results form wikipedia API.");
+  }
+
+  const json = await reponse.json();
+  return json;
+}
