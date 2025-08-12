fac382803dbdcebff653c2704541dd1602c5d844
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-12 23:12:30 +0530

    grayHeart.addEventListener is added

--- SWP15/app.js
+++ SWP15/app.js
@@ -1,2 +1,7 @@
 const grayHeart = document.querySelector(".gray-heart");
 const redHeart = document.querySelector(".red-heart");
+
+grayHeart.addEventListener("click", () => {
+  redHeart.classList.add("animation");
+  grayHeart.classList.add("fill-color");
+});
