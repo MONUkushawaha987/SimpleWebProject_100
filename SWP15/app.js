ea0eaa638a49a7a23fa9657d8d23754d9e62f7c4
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-12 23:13:01 +0530

    redHeart.addEventListener is added

--- SWP15/app.js
+++ SWP15/app.js
@@ -5,3 +5,8 @@ grayHeart.addEventListener("click", () => {
   redHeart.classList.add("animation");
   grayHeart.classList.add("fill-color");
 });
+
+redHeart.addEventListener("click", () => {
+  redHeart.classList.remove("animation");
+  grayHeart.classList.remove("fill-color");
+});
