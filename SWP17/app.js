24805190106be05424e1226fb1c1ac7c3f2bb77f
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-18 19:20:01 +0530

    words and typingDelay variable is added in app.js file

--- SWP17/app.js
+++ SWP17/app.js
@@ -1,2 +1,5 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursor = document.querySelector(".cursor");
+
+const words = ["Awesome", "Fun", "Cool", "Life", "Famous", "Weird"];
+const typingDelay = 200;
