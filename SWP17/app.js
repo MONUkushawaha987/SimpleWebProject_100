98d6ab04d5b6fbbf2224e5dd42ac21d4dbeda098
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-19 23:39:36 +0530

    DOMContentLoaded is added in app.js

--- SWP17/app.js
+++ SWP17/app.js
@@ -7,3 +7,8 @@ const erasingDelay = 200;
 const newLetterDelay = 2000;
 let index = 0;
 let charIndex = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  if (words.length) {
+    setTimeout(type, newLetterDelay);
+  }
+});
