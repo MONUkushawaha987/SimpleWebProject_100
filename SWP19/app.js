fd67c9b413c2a5f1d2bd53ce6a533889e4432633
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-24 14:22:58 +0530

    const variable video is added in app.js file

--- SWP19/app.js
+++ SWP19/app.js
@@ -1,3 +1,4 @@
 const btn = document.querySelector(".btn");
 const closeIcon = document.querySelector(".close-icon");
 const trailerContainer = document.querySelector(".trailer-container");
+const video = document.querySelector("video");
