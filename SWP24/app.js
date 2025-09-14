cf66b235c56b10bebcef70029cfa552cfb168a1b
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-09-14 17:54:16 +0530

    four variable is created

---
+++ SWP24/app.js
@@ -0,0 +1,4 @@
+ let num1 = document.querySelector(".num1").value;
+    let num2 = document.querySelector(".num2").value;
+    let result = document.querySelector(".result");
+    let oprator = document.getElementById("selectOp").value;
