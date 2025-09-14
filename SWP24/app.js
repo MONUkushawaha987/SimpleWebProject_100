c7527b5f6e14373d409b4eaccf6265891d4466dc
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-09-14 17:57:26 +0530

    eventlistener is added in app.js file

--- SWP24/app.js
+++ SWP24/app.js
@@ -1,4 +1,5 @@
- let num1 = document.querySelector(".num1").value;
+    document.getElementById("btn").addEventListener("click", function () {
+    let num1 = document.querySelector(".num1").value;
     let num2 = document.querySelector(".num2").value;
     let result = document.querySelector(".result");
     let oprator = document.getElementById("selectOp").value;
@@ -16,3 +17,4 @@
       case "multi":
         result.innerHTML = Number(num1) * Number(num2);
     }
+});
