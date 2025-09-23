3e4221ebc93226b8c759f60d19c2909735739fd9
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-09-23 22:16:58 +0530

    copytext , finaltext, movebtn, output variables is added in app.js file

---
+++ SWP25/app.js
@@ -0,0 +1,5 @@
+const copyText = document.querySelector("textarea[name=copyTxt");
+const finalText = document.querySelector("textarea[name=finalTxt");
+const moveBtn = document.querySelector(".moverBtn");
+const copyBtn = document.querySelector(".copyBtn");
+const output = document.querySelector(".output");
