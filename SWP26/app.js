503a60c07ccd6a637d25d4765cc5be75431e715b
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-09-26 14:53:36 +0530

    four variables (progress,preBtn,nextBtn,circles) are added in app.js file

---
+++ SWP26/app.js
@@ -0,0 +1,4 @@
+const progress = document.getElementById("progress");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
