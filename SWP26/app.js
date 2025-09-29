8632692147867a9ecf03a5eb2b07472e8f592295
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-09-29 21:47:13 +0530

    eventListener on click with nextBtn is added in app.js file

--- SWP26/app.js
+++ SWP26/app.js
@@ -2,3 +2,14 @@ const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
+
+let activeIndex = 1;
+
+nextBtn.addEventListener("click", () => {
+  activeIndex++;
+  if (activeIndex > circles.length) {
+    activeIndex = circles.length;
+  }
+
+  updateUI();
+});
