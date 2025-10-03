eeb5dcf08d3f3e1524c8e926f75538d1e89f40d9
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-10-03 17:57:45 +0530

    click event Handler on window  is added in app.js file

--- SWP27/app.js
+++ SWP27/app.js
@@ -4,3 +4,7 @@ const modal = document.getElementById("modal");
 
 open.addEventListener("click", () => modal.classList.add("show-modal"));
 close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+window.addEventListener("click", (e) => {
+  e.target === modal ? modal.classList.remove("show-modal") : false;
+});
