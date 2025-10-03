758bada6c0e238a500d67cd81dda63a0429656da
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-10-03 17:56:40 +0530

    click eventhandler is added on open and close  is added in app.js file

--- SWP27/app.js
+++ SWP27/app.js
@@ -2,3 +2,5 @@ const close = document.getElementById("close");
 const open = document.getElementById("open");
 const modal = document.getElementById("modal");
 
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
