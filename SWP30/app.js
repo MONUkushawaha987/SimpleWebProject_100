1db193840a30a0b44fb4e952dd2b9fc947059e66
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-11-09 18:16:22 +0530

    options variable is created

--- SWP30/app.js
+++ SWP30/app.js
@@ -2,4 +2,5 @@ let selectField = document.getElementById("selectField");
 let selectText = document.getElementById("selectText");
 let list = document.getElementById("list");
 let arrowIcon = document.getElementById("arrowIcon");
+let options = document.getElementsByClassName("options");
 
