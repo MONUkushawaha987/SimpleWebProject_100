7031a70791a131a23344ddd975750c208b48d57b
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-11-27 19:14:41 +0530

    menu links highlight and btn variables are added in app.js file

---
+++ SWP32/app.js
@@ -0,0 +1,4 @@
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll("li");
+const highlight = document.querySelector(".highlight");
+const btn = document.querySelector(".btn");
