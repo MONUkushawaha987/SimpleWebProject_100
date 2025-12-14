99358f495e51411010a19e1602fadef81a292f5d
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-12-14 17:31:01 +0530

    variables of js is added in app.js file

---
+++ SWP33/app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".page-container");
+const pages = document.querySelectorAll(".page");
+const toggleBtn = document.querySelector(".toggle-btn");
+const ul = document.querySelector(".nav-list");
+const overlay = document.querySelector(".overlay");
+const links = document.querySelectorAll(".link");
